2cf72a4403f2267c23b05a3ebeaa361691976025
Chloe Gell <gellert.chloe@gmail.com>
2020-01-28 22:14:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     str1 = season.to_s.capitalize! 
     puts "#{str1}:"
     holiday.each do |event, supplies|
-      puts " #{event.to_s.split("_")}: #{flattened}"
+      puts " #{event.to_s.split("_").map each do |x| x.capitalize!}: #{flattened}"
       binding.pry 
     end 
   end 
